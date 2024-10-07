ed9275a1b15d3db2a9b36dba02d92db6325a96e1
EM40a <hermosilla.emanuel811@gmail.com>
2024-10-07 22:18:43 +0000

    fix punto a, c y d

--- puntod.sh
+++ puntod.sh
@@ -1,3 +1,5 @@
-  106  mkdir -p Estructura_Asimetrica/{{correo,cliente}/cartas_{01..100},correo/carteros_{01..10}}
-  107  tree Estructura_Asimetrica/
-  108  history |tail -n 3 > puntod.sh
+mkdir -p Estructura_Asimetrica/{{correo,cliente}/cartas_{01..100},correo/carteros_{01..10}}
+apt install tree
+tree Estructura_Asimetrica/
+
+
